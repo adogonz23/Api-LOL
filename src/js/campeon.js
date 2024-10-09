5985e1215f824813a05edaf73739ca056b1f9dcc
adogonz23 <adogonzalez75@gmail.com>
2024-10-09 19:54:47 +0100

    a

--- src/js/campeon.js
+++ src/js/campeon.js
@@ -4,6 +4,7 @@ export class Campeon {
         this.imagen= "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/"+this.name+"_0.jpg";
         this.splasArt="http://ddragon.leagueoflegends.com/cdn/img/champion/splash/"+this.name+"_0.jpg";
         this.titulo = data.title;
+        console.log(this.titulo);
     }
     
 }
